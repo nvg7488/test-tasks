f88ee6ce28c11e7fe9c78f30e444fc2b618df17a
nvg7488 <govoruchin_nv@mail.ru>
2019-03-27 15:15:55 +0700

    1 Стажёр. 3 Collection Lite. Departments

--- job4jTrainee/src/main/java/ru/govoruchin/chapter003collections/Departments.java
+++ job4jTrainee/src/main/java/ru/govoruchin/chapter003collections/Departments.java
@@ -1,9 +1,6 @@
 package ru.govoruchin.chapter003collections;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Тестовая задача: Отсортировать департаменты.
@@ -16,34 +13,55 @@ import java.util.TreeSet;
  */
 public class Departments {
     public String[] sortInc(String[] departments) {
+        String[] toSort = this.missing(departments);
         Set<String> sorted = new TreeSet<>(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return o1.compareTo(o2);
             }
         });
-        sorted.addAll(Arrays.asList(departments));
-        String[] result = new String[sorted.size()];
-        return sorted.toArray(result);
+        sorted.addAll(Arrays.asList(toSort));
+        String[] resultArray = new String[sorted.size()];
+        return sorted.toArray(resultArray);
     }
 
     public String[] sortDec(String[] departments) {
+        String[] toSort = this.missing(departments);
         Set<String> sorted = new TreeSet<>(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                int min = Math.min(o1.length(), o2.length());
-                for (int index = 0; index < min; index++) {
-                    Character left = o1.charAt(index);
-                    Character right = o2.charAt(index);
-                    if (right.compareTo(left) != 0) {
-                        return right - left;
+                int minimal = Math.min(o1.length(), o2.length());
+                for (int index = 0; index < minimal; index++) {
+                    Character leftChar = o1.charAt(index);
+                    Character rightChar = o2.charAt(index);
+                    if (rightChar.compareTo(leftChar) != 0) {
+                        return rightChar - leftChar;
                     }
                 }
                 return o1.length() - o2.length();
             }
         });
-        sorted.addAll(Arrays.asList(departments));
+        sorted.addAll(Arrays.asList(toSort));
         String[] result = new String[sorted.size()];
         return sorted.toArray(result);
+    }
+
+    public String[] missing(String[] departments) {
+        Set<String> departmentsSet = new TreeSet<>();
+        departmentsSet.addAll(Arrays.asList(departments));
+        for (String department : departments) {
+            ArrayList<Integer> positions = new ArrayList<>();
+            for (int position = 0; position < department.length(); position++) {
+                if (department.charAt(position) == '\\') {
+                    positions.add(position);
+                }
+            }
+            for (Integer position : positions) {
+                char[] departmentAsChar = department.toCharArray();
+                String code = new String(departmentAsChar, 0, position);
+                departmentsSet.add(code);
+            }
+        }
+        return departmentsSet.toArray(new String[departmentsSet.size()]);
     }
 }
--- job4jTrainee/src/test/java/ru/govoruchin/chapter003collections/DepartamentsTest.java
+++ job4jTrainee/src/test/java/ru/govoruchin/chapter003collections/DepartamentsTest.java
@@ -19,11 +19,13 @@ public class DepartamentsTest {
                 "K2\\SK1\\SSK2"};
         Departments departments = new Departments();
         String[] expected = {
+                "K1",
                 "K1\\SK1",
                 "K1\\SK1\\SSK1",
                 "K1\\SK1\\SSK2",
                 "K1\\SK2",
                 "K2",
+                "K2\\SK1",
                 "K2\\SK1\\SSK1",
                 "K2\\SK1\\SSK2"};
         assertThat(departments.sortInc(array), is(expected));
@@ -42,8 +44,10 @@ public class DepartamentsTest {
         Departments departments = new Departments();
         String[] expected = {
                 "K2",
+                "K2\\SK1",
                 "K2\\SK1\\SSK2",
                 "K2\\SK1\\SSK1",
+                "K1",
                 "K1\\SK2",
                 "K1\\SK1",
                 "K1\\SK1\\SSK2",
